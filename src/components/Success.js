3464b674b759146b2abce5c1ac702279fbf5d21a
rossdack <34684873+rossdack@users.noreply.github.com>
2018-05-06 19:22:05 +1000

    //

--- src/components/Success.js
+++ src/components/Success.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 class Success extends Component {
 
     render() {
-        console.log(this.props);
         return (
             <div>
                 <h3>All done!</h3>
